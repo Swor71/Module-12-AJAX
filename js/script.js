63302976620402c5e214205603995f395b59ec33
Marcin Swornowski <swor71@gmail.com>
2018-01-18 15:42:01 +0100

    Changed file sys to jQuery

--- js/script.js
+++ js/script.js
@@ -1,19 +1,18 @@
 var url = 'http://api.icndb.com/jokes/random';
 
-var button = document.getElementById('get-joke');
-button.addEventListener('click', function(){
+var $button = $('#get-joke').click(function() {
 	getJoke();
 });
 
-var paragraph = document.getElementById('joke');
+var $paragraph = $('#joke');
 
 function getJoke() {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', url);
-	xhr.addEventListener('load', function(){
-		var response = JSON.parse(xhr.response);
-		paragraph.innerHTML = response.value.joke;
+	$.ajax({
+		url: url,
+		method: 'GET',
+		success: function(res) {
+			$paragraph.text(res.value.joke);
+		}
 	});
-	xhr.send();
 }
 getJoke();
